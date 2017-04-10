0ba4cdc6dab5aeb29403aee6380a9d1762072f32
Cristina Iannone <iannonecristina@gmail.com>
2017-04-10 16:28:09 +0100

    Update spec file

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,4 +1,10 @@
 require './lib/DockingStation.rb'
 
 describe DockingStation do
+  it { expect(docking_station(release_bike)).to eq nil }
+
+#  it 'responds to the method release_bike' do
+#    expect(docking_station(release_bike)).to eq nil
+# end
+
 end
