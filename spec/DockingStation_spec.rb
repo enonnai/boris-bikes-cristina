775dc5fb1d405a8091b5499919637fb23aeea9a5
milesillsley <milesillsley@hotmail.co.uk>
2017-04-10 19:12:13 +0100

    rspec test

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,10 +1,11 @@
 require './lib/DockingStation.rb'
-
+require './lib/Bike.rb'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
 #  it 'responds to the method release_bike' do
 #    expect(docking_station(release_bike)).to eq nil
 # end
-
+  it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:release_bike) }
 end
