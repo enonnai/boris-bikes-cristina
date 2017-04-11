2a90776a4ed4cd0c68444eadb806f98612d6e955
Cristina Iannone <iannonecristina@gmail.com>
2017-04-11 12:20:48 +0100

    Updates the test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,5 @@
 # in lib/docking_sation.rb
-require './lib/Bike.rb'
+require './lib/bike.rb'
 
 class DockingStation
   def release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,13 @@
-require './lib/DockingStation'
-require './lib/Bike'
-
+require './lib/docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to :release_bike }
+
+  it 'gets a bike' do
+  expect(subject.release_bike).to be_an_instance_of Bike
+  end
+
+ # it 'expects the bike to be working' do
+ # end
 
 #  it 'responds to release_bike' do
 # expect(subject).to respond_to :release_bike
