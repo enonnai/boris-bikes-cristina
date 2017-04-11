c870107b1ccf6d1d5d9f3b07fd74d0e7ce249583
Cristina Iannone <iannonecristina@gmail.com>
2017-04-11 12:33:17 +0100

    Bike fails working? test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,15 @@
 require './lib/docking_station'
+
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
 
   it 'gets a bike' do
-  expect(subject.release_bike).to be_an_instance_of Bike
+    expect(subject.release_bike).to be_an_instance_of Bike
   end
 
- # it 'expects the bike to be working' do
- # end
+  it 'expects the bike to be working' do
+    expect(subject.release_bike).to be_working
+  end
 
-#  it 'responds to release_bike' do
-# expect(subject).to respond_to :release_bike
-# end
 end
