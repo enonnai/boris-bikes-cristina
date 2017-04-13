6d4e76cef478ddf46f641aad361e4ba5b9045d8c
Robert Jones <robert.g.jones@gmail.com>
2017-04-13 15:38:20 +0100

    Check bike.broken? returns false by default

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,5 +3,8 @@ require './lib/bike.rb'
 describe Bike do
   it { is_expected.to respond_to(:working?) }
   it { is_expected.to respond_to(:report_broken) }
+  it "Returns false as default when passed the broken? method" do
+    expect(subject.broken?).to eq false
+  end
 
 end
