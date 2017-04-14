b4ff7f966394398fc725b6f3d280829cbc919cbe
Cristina Iannone <iannonecristina@gmail.com>
2017-04-14 11:00:58 +0100

    Returns a broken_bike when called broken?

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,14 +1,18 @@
 class Bike
+  def initialize
+    @broken_bikes = false
+  end
 
   def working?
     true
   end
 
   def report_broken
+    @broken_bikes = true
   end
 
   def broken?
-    false
+    @broken_bikes
   end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,13 +2,15 @@ require './lib/bike.rb'
 
 describe Bike do
   it { is_expected.to respond_to(:working?) }
+
   it { is_expected.to respond_to(:report_broken) }
+
   it "Returns false as default when passed the broken? method" do
     expect(subject.broken?).to eq false
   end
 
   describe '#broken?' do
-      it "returns true if bike.report_broken has been run" do
+      it "returns true if bike.report_broken has been called" do
         subject.report_broken
         expect(subject.broken?).to eq true
       end
